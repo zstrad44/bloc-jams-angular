bd4d31daf103c3299a1f8e62e125bd380fefeea8
Zach Stradling <zachstradling@gmail.com>
2016-10-10 14:45:30 -0700

    Assignment 6 complete

--- app/scripts/services/Fixtures.js
+++ app/scripts/services/Fixtures.js
@@ -36,6 +36,14 @@
             return albumPicasso;
         };
         
+       Fixtures.getCollection = function(numberOfAlbums) {
+           this.albums = [];
+                for (var i = 0; i < numberOfAlbums; i++) {
+                    this.albums.push(angular.copy(albumPicasso));
+                }
+           return this.albums;
+       };
+        
         return Fixtures;
     }
     
