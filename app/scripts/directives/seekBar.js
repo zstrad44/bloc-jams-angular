50a638b9b74aaf7e24147d6c6335d03b853c663c
Zach Stradling <zachstradling@gmail.com>
2016-10-11 12:16:17 -0700

    assignment 9 complete

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -49,6 +49,10 @@
                         $document.unbind('mouseup.thumb');
                     });
                 };
+                
+                scope.thumbStyle = function() {
+                    return {left: percentString()};
+                };
             }
         };
         
